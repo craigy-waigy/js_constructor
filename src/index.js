6c01d11ad9f2049c543eab587489072fad8b4551
Dmitry Shilin <waigy.craigy@gmail.com>
2020-10-25 03:37:13 +0500

    add blocks classes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import {model} from './model'
-import {templates} from './templates'
 import './styles/main.css'
 
 const $site = document.querySelector('#site')
 
 model.forEach(block => {
-    const toHTML = templates[block.type]
 
-    console.log(toHTML)
+    console.log(block)
 
-    if (toHTML) {
-        $site.insertAdjacentHTML('beforeend', toHTML(block))
-    }
+    $site.insertAdjacentHTML('beforeend', block.toHTML())
+
 })
--- src/model.js
+++ src/model.js
@@ -1,12 +1,34 @@
+import image from './assets/image.png'
+import {TextBlock, TitleBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
+
 export const model = [
-    {type: 'title', value: 'Hello World from JS!'},
-    {type: 'text', value: 'here we go with some text'},
-    {type: 'columns', value: [
-            '111111111',
-            '222222222',
-            '222222222',
-            '222222222',
-            '333333333'
-        ]},
-    {type: 'image', value: './assets/image.png'}
+    new TitleBlock('Конструктор сайтов на JS', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center'
+        }
+    }),
+    new TextBlock('here we go with some text', {}),
+    new ColumnsBlock([
+        '111111111',
+        '222222222',
+        '222222222',
+        '222222222',
+        '333333333'
+    ], {}),
+    new ImageBlock(image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imageStyles: {
+            width: '500px',
+            height: 'auto'
+        },
+        alt: 'Это картинка'
+    }),
 ]
